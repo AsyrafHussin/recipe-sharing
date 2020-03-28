5f43d47d3e1d4873f9b2ef5fd3bb5e785eb04440
Asyraf Hussin <asyrafhussin4@gmail.com>
2020-03-28 23:53:46 +0800

    Add Styled Components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
+import styled from "styled-components";
+
+const StyledHeader = styled.h1`
+  font-size: 2em;
+  text-align: left;
+  padding: 16px 16px;
+`;
+
 const Index = () => {
-    return (
-        <div>
-            <p>My Index page</p>
-        </div>
-    );
+  return (
+    <div>
+      <StyledHeader>My Index page</StyledHeader>
+    </div>
+  );
 };
 
 export default Index;

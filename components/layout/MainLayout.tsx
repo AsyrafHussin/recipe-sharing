94bd20ae02317967f6570d4a63397237f628b263
Asyraf Hussin <asyrafhussin4@gmail.com>
2020-04-04 07:40:55 +0800

    Add Global Style

--- components/layout/MainLayout.tsx
+++ components/layout/MainLayout.tsx
@@ -1,8 +1,14 @@
 import { ThemeProvider } from "styled-components";
 import { theme } from "../../utils/theme";
+import { GlobalStyle } from "../../utils/globalStyle";
 
 const MainLayout = ({ children }) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      {children}
+    </ThemeProvider>
+  );
 };
 
 export default MainLayout;
